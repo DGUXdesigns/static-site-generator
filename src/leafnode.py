805a58143a75acadec3a8d142c6c5ab24e4d7401
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2026-01-09 12:43:29 -0500

    add leafnode class

---
+++ src/leafnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str,
+        value: str,
+        props: dict | None = None,
+    ) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("Invalid HTML: no Value")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, props: {self.props})"
