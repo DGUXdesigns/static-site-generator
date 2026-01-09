4979d2d7790c3454989467e62d9906fff17c9049
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2026-01-09 13:39:19 -0500

    add ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,27 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, props: {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list, props: dict | None = None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+
+        result = f"<{self.tag}{self.props_to_html()}>"
+
+        for child in self.children:
+            result += child.to_html()
+
+        result += f"</{self.tag}>"
+
+        return result
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
