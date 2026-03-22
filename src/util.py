10e89b6b9230c655a90d0c7f8736785351fba4e2
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2026-03-22 17:33:16 -0400

    feat: add text_to_textnodes function

--- src/util.py
+++ src/util.py
@@ -96,3 +96,13 @@ def split_nodes_link(old_nodes: list) -> list:
             result.append(TextNode(text, TextType.TEXT))
 
     return result
+
+
+def text_to_textnodes(text: str) -> list:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
