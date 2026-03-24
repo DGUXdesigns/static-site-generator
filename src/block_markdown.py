dd1bf48e2e2bbec30d8923a36ebd47706c71d542
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2026-03-23 20:22:26 -0400

    feat: implement block type classification for markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,16 @@
-def markdown_to_blocks(markdown) -> list:
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown: str) -> list:
     blocks = markdown.split("\n\n")
     filtered_blocks = []
 
@@ -10,3 +22,35 @@ def markdown_to_blocks(markdown) -> list:
         filtered_blocks.append(block)
 
     return filtered_blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+    # Headings
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    # Code
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    # Quote
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    # Unordered list
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    # Ordered List
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+
+    return BlockType.PARAGRAPH
